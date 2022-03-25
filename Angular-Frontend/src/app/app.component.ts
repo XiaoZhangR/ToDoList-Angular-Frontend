981d81a53bc3b223a5a06ff1b106ab781e03a6ff
Xiao Zhang <xz9rc@umsystem.edu>
2022-03-25 14:14:15 -0500

    angular front end for todolist app

---
+++ Angular-Frontend/src/app/app.component.ts
@@ -0,0 +1,72 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+export interface ThingsToDo {
+  id:number;
+  item: string;
+  status: boolean
+}
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  title = 'EmailTemplate';
+  displayedColumns: string[] = ['id', 'item', 'status', 'edit', 'delete'];
+  dataSource: any = [];
+
+  dataset: any = {
+    id: null,
+    item: '',
+    status: null
+  };
+
+  constructor(private https: HttpClient){
+    
+   }
+  
+  sendEmail() {
+    this.https.get(`http://localhost:8080/api/email/${this.dataset.item}/${this.dataset.status}`).subscribe(
+      res => {
+        console.log(res);
+      }
+    )
+  }
+
+  // onSubmit() {
+
+  //   this.https.put<any>('http://localhost:8080/api/thingstodo', this.dataset).subscribe(
+  //     res=>{
+  //       // console.log(res);
+  //       this.dataset.item = '';
+  //       this.dataset.status = null;
+  //     }
+  //   )
+  // }
+
+  ngOnInit() {
+    // this.https.get('http://localhost:8080/api/thingstodo').subscribe(res => {
+    //     // console.log(res[0].id);
+    //     this.dataSource = res;
+    //   })
+  }
+
+  // deleteItem(key: number) {
+  //   // console.log(key);
+  //   this.https.delete(`http://localhost:8080/api/thingstodo/${key}`).subscribe(() => {
+  //       console.log('deleted');
+  //   })
+  // }
+
+  // editItem(key: number) {
+  //   this.https.get(`http://localhost:8080/api/thingstodo/${key}`).subscribe(
+  //     res => {
+  //       this.dataset = res;
+  //     }
+  //   )
+  // }
+
+}
