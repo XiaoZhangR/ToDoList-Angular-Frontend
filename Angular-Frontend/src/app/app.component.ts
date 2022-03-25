f973ff3ce82d10de4b32e9e21ee61d4888d5b150
Xiao Zhang <xz9rc@umsystem.edu>
2022-03-25 15:23:04 -0500

    init CRUD

--- Angular-Frontend/src/app/app.component.ts
+++ Angular-Frontend/src/app/app.component.ts
@@ -28,45 +28,45 @@ export class AppComponent implements OnInit{
     
    }
   
-  sendEmail() {
-    this.https.get(`http://localhost:8080/api/email/${this.dataset.item}/${this.dataset.status}`).subscribe(
-      res => {
-        console.log(res);
+  // sendEmail() {
+  //   this.https.get(`http://localhost:8080/api/email/${this.dataset.item}/${this.dataset.status}`).subscribe(
+  //     res => {
+  //       console.log(res);
+  //     }
+  //   )
+  // }
+
+  onSubmit() {
+
+    this.https.put<any>('http://localhost:8080/api/thingstodo', this.dataset).subscribe(
+      res=>{
+        // console.log(res);
+        this.dataset.item = '';
+        this.dataset.status = null;
       }
     )
   }
 
-  // onSubmit() {
-
-  //   this.https.put<any>('http://localhost:8080/api/thingstodo', this.dataset).subscribe(
-  //     res=>{
-  //       // console.log(res);
-  //       this.dataset.item = '';
-  //       this.dataset.status = null;
-  //     }
-  //   )
-  // }
-
   ngOnInit() {
-    // this.https.get('http://localhost:8080/api/thingstodo').subscribe(res => {
-    //     // console.log(res[0].id);
-    //     this.dataSource = res;
-    //   })
+    this.https.get('http://localhost:8080/api/thingstodo').subscribe(res => {
+        // console.log(res[0].id);
+        this.dataSource = res;
+      })
   }
 
-  // deleteItem(key: number) {
-  //   // console.log(key);
-  //   this.https.delete(`http://localhost:8080/api/thingstodo/${key}`).subscribe(() => {
-  //       console.log('deleted');
-  //   })
-  // }
+  deleteItem(key: number) {
+    // console.log(key);
+    this.https.delete(`http://localhost:8080/api/thingstodo/${key}`).subscribe(() => {
+        console.log('deleted');
+    })
+  }
 
-  // editItem(key: number) {
-  //   this.https.get(`http://localhost:8080/api/thingstodo/${key}`).subscribe(
-  //     res => {
-  //       this.dataset = res;
-  //     }
-  //   )
-  // }
+  editItem(key: number) {
+    this.https.get(`http://localhost:8080/api/thingstodo/${key}`).subscribe(
+      res => {
+        this.dataset = res;
+      }
+    )
+  }
 
 }
